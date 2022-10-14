fceb7589445574780a74cafa6459e69f8b4e2a4a
osmanbastos <osmanbastos@hotmail.com>
2022-10-14 15:14:10 -0300

    new file:   index.html 	new file:   main.js 	new file:   style.css

---
+++ main.js
@@ -0,0 +1,20 @@
+function calcular(){
+    var peso = document.getElementById('peso')
+    var altura = document.querySelector('input#altura')
+    var res = document.querySelector('div#res')
+    var peso1 = Number(peso.value)
+    var altura1 = Number(altura.value)
+    var imc = Number.parseInt(peso1/(altura1*altura1))
+    if (imc < 25){
+        res.innerHTML = `O seu IMC é igual <b>${imc}</b>, você está no seu peso normal`
+    } else if (25 <= imc & imc <30){
+        res.innerHTML = `O seu IMC é igual <b>${imc}</b>, você está com sobrepeso`
+    } else if (imc >= 30 & imc < 40){
+        res.innerHTML = `O seu IMC é igual <b>${imc}</b>, você está com obesidade`
+    } else if (40 < imc){
+        res.innerHTML = `O seu IMC é igual <b>${imc}</b>, você está com obesidade grave!`
+    }else {
+        res.innerHTML = "Dados incorretos"
+    }
+}
+   
